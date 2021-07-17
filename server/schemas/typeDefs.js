f998f61a77dc3a9410f8a0d0de3352e74e25bdf9
Stevtm <steventmaddison@outlook.com>
2021-07-16 20:19:24 -0400

    complete typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,6 +10,32 @@ const typeDefs = gql`
         bookCount = Int
         savedBooks [Book]
     }
+
+    type Book {
+        _id: ID
+        bookId: String
+        authors: [String] 
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(author: [String], description: String!, bookId: String!, image:    String!, link: String!): User
+        removeBook(bookId: String!): User
+    }
 `;
 
 // export typeDefs

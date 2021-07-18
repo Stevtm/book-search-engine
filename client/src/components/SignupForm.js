4483c2e90743a0437750d6c32f36278e53ad6eba
Stevtm <steventmaddison@outlook.com>
2021-07-18 14:42:13 -0400

    reformat config file

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -41,14 +41,6 @@ const SignupForm = () => {
 				variables: { ...userFormData },
 			});
 
-			// const response = await createUser(userFormData);
-
-			// if (!response.ok) {
-			// 	throw new Error("something went wrong!");
-			// }
-
-			// const { token, user } = await response.json();
-			// console.log(user);
 			Auth.login(data.addUser.token);
 		} catch (err) {
 			console.error(err);

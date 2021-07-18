e381730458b2760f9d3ea62ea98e7518ada47d0d
Stevtm <steventmaddison@outlook.com>
2021-07-17 23:27:18 -0400

    develop and test mutation to remove book from user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -63,6 +63,19 @@ const resolvers = {
 
 			throw new AuthenticationError("You need to be logged in!");
 		},
+		removeBook: async (parent, { bookId }, context) => {
+			if (context.user) {
+				const updatedUser = await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{ $pull: { savedBooks: { bookId } } },
+					{ new: true, runValidators: true }
+				);
+
+				return updatedUser;
+			}
+
+			throw new AuthenticationError("You need to be logged in!");
+		},
 	},
 };
 
